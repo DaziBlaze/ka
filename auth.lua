5854899b02b924fec8ef26f22fbe9b8ac4da74ef
DaziBlaze <ltbrosens@gmail.com>
2025-10-23 02:19:10 +0200

    Update auth.lua

--- auth.lua
+++ auth.lua
@@ -13,9 +13,6 @@ local initData = HttpService:JSONDecode(initReq)
 if not initData.success then return end
 local sessionid = initData.sessionid
 
--- get local HWID
-local HWID = Analytics:GetClientId()
-
 -- check license
 local licenseUrl = 'https://keyauth.win/api/1.1/?name=' .. name ..
                    '&ownerid=' .. ownerid ..
@@ -26,20 +23,5 @@ local licenseUrl = 'https://keyauth.win/api/1.1/?name=' .. name ..
 local licenseReq = game:HttpGet(licenseUrl)
 local data = HttpService:JSONDecode(licenseReq)
 if not data.success then return end
-
--- if the license HWID variable is empty, set it
-local hwidVar = data.info.var or "" -- var is the KeyAuth variable for custom data
-if hwidVar == "" then
-    local setHWIDUrl = 'https://keyauth.win/api/1.1/?name=' .. name ..
-                       '&ownerid=' .. ownerid ..
-                       '&type=setvar&var=hwid&varval=' .. HttpService:UrlEncode(HWID) ..
-                       '&sessionid=' .. sessionid
-    game:HttpGet(setHWIDUrl)
-end
-
--- re-fetch var if needed
-local licenseReq2 = game:HttpGet(licenseUrl)
-local data2 = HttpService:JSONDecode(licenseReq2)
-if data2.info.var ~= HWID then return end
 
 print("🔑 Successfully whitelisted by Hardbuild")
